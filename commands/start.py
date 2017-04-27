1ad1aaf52665897289facdc26c5a3d187354e841
Carlovan <giulio.carlassare@gmail.com>
2017-04-27 19:26:09 +0200

    Now /start checks if user wxists before adding

--- commands/start.py
+++ commands/start.py
@@ -7,5 +7,6 @@ def run(bot, update):
 	text = utils.help.main_text
 	text += '\n Puoi leggere nuovamente questo messaggio con /help .'
 	user = update.message.from_user
-	database.users.add_new(utils.user.User(user.id, user.username, False))
+	if database.users.get_single(utils.filters.UserFilter(userid=user.id)) == None:
+		database.users.add_new(utils.user.User(user.id, user.username, False))
 	update.message.reply_text(text)
