36a1dd4429d01cf16e29fd21686e3c798ef1ef10
Carlovan <giulio.carlassare@gmail.com>
2017-04-30 18:29:51 +0200

    Fixed mostra message was creating the Inventory without user

--- messages/mostra.py
+++ messages/mostra.py
@@ -7,6 +7,6 @@ import utils.user
 def run(bot, update, groupdict):
 	if utils.is_from_ib(update):
 		user = utils.user.User.from_telegram(update.message.from_user)
-		inv = utils.inventory.Inventory([utils.item.Item.from_string(groupdict['item'])])
+		inv = utils.inventory.Inventory([utils.item.Item.from_string(groupdict['item'])], user)
 		count = utils.inventory.received(inv)
 		update.message.reply_text(f'Hai aggiunto {count} oggetti.')
