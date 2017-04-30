690128021f03a615a606fd8e35ad07566634d8ba
Carlovan <giulio.carlassare@gmail.com>
2017-04-30 15:04:12 +0200

    Added support to the /esporta file

--- messages/__init__.py
+++ messages/__init__.py
@@ -2,12 +2,18 @@
 # This package contains handlers for text messages
 
 import importlib
-from telegram.ext import RegexHandler
+from telegram.ext import MessageHandler, RegexHandler
+from telegram.ext.filters import Filters
 
-msg_list = [('inventario','@.*, possiedi( \(pg \d+/\d+\))?:\n.*'), ('mostra', '@.*, mostri orgogliosamente di possedere \d+x (?P<item>.*)')]
+msg_list = [('inventario','@.*, possiedi( \(pg \d+/\d+\))?:\n.*'),
+            ('mostra', '@.*, mostri orgogliosamente di possedere \d+x (?P<item>.*)'),
+            ('esporta', Filters.document)]
 
 handlers = []
 for c in msg_list:
 	m = importlib.import_module('.'+c[0], __name__)
 	groups = m.run.__code__.co_argcount == 3 # If the run function takes 3 arguments, groupdict from the regex is required
-	handlers.append(RegexHandler(c[1], m.run, pass_groupdict=groups))
+	if type(c[1]) is str:
+		handlers.append(RegexHandler(c[1], m.run, pass_groupdict=groups))
+	else:
+		handlers.append(MessageHandler(c[1], m.run))
---
+++ messages/esporta.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Handles an inventory message
+
+import utils.inventory
+import utils.user
+import os
+import time
+import re
+
+def run(bot, update):
+	if utils.is_from_ib(update):
+		user = utils.user.User.from_telegram(update.message.from_user)
+		doc = bot.getFile(update.message.document.file_id)
+		filename = str(time.time()) + '.txt'
+		doc.download(filename)
+		with open(filename, 'r') as ff:
+			content = ff.read()
+		os.remove(filename)
+		content = re.sub(r'[^\S\n]+', ' ', content) # Replace any group of consecutive blanks (except newline) with a single whitespace
+		content = re.sub(r'^(\d)+x +(.+)$', r'- \2 x\1', content, flags=re.MULTILINE) # makes every lline in the same format as the command /inventario
+		update.message.reply_text(content[:1000])
+		inv = utils.inventory.Inventory.parse(content, user)
+		count = utils.inventory.received(inv)
+		update.message.reply_text(f'Hai aggiunto {count} oggetti.')
