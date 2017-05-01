87822340f0ef53ca0eb49ebc62f5f53c3eca60b1
Carlovan <giulio.carlassare@gmail.com>
2017-05-01 19:02:56 +0200

    Better help

--- commands/helpcomandi.py
+++ commands/helpcomandi.py
@@ -4,4 +4,6 @@ import utils.help
 
 def run(bot, update):
 	text = utils.help.commands_text
+	if utils.is_from_admin(update):
+		text += '\n' + utils.help.admin_commands_text
 	update.message.reply_text(text, parse_mode='HTML')
