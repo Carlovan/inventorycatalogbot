112d34baf86847a5b2734645fe1102a03c698e18
Carlovan <giulio.carlassare@gmail.com>
2017-05-02 13:03:09 +0000

    Now /start updates user if already present

--- commands/start.py
+++ commands/start.py
@@ -2,11 +2,15 @@
 
 import utils.help
 import utils.user, database.users
+from utils.filters import UserFilter
 
 def run(bot, update):
 	text = utils.help.main_text
 	text += '\n Puoi leggere nuovamente questo messaggio con /help .'
-	user = update.message.from_user
-	if database.users.get_single(utils.filters.UserFilter(userid=user.id)) == None:
+	user = database.users.get_single(UserFilter(userid=update.message.from_user.id))
+	if user == None:
 		database.users.add_new(utils.user.User.from_telegram(update.message.from_user))
+	else:
+		user.username = update.message.from_user.username
+		database.users.update(user)
 	update.message.reply_text(text, parse_mode='HTML')
