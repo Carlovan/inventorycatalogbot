3016733ce52d8266d86e39df77a342439c0f2d3e
Carlovan <giulio.carlassare@gmail.com>
2017-05-03 21:55:59 +0200

    Moved changelog on a different file

--- commands/start.py
+++ commands/start.py
@@ -3,6 +3,7 @@
 import utils.help
 import utils.user, database.users
 from utils.filters import UserFilter
+import settings
 import logging
 logger = logging.getLogger(__name__)
 
@@ -12,14 +13,7 @@ def run(bot, update):
 	dbusers = database.users.DbUsers()
 	#text = utils.help.main_text
 	#text += '\n Puoi leggere nuovamente questo messaggio con /help .'
-	text = '''Changelog felice!
-Il bot e' stato completamente riscritto anche se da fuori non sembrerebbe. Il codice lo trovate <a href="https://github.com/Carlovan/inventorycatalogbot">qui</a>.
-Ora sono supportati anche i comandi /inv2 e /esporta di @InventoryBot.
-Il comando /confronta dovrebbe funzionare molto meglio rispetto a prima.
-Ho anche cambiato server quindi non dovrete piu aspettare i soliti 10 secondi ogni volta che gli scrivete :)
-
-Per info generali /help. Per info sui comandi (consigliata la lettura) /helpcomandi.
-Per segnalazioni @Carlovan.'''
+	text = settings.changelog
 	user = dbusers.get_single(UserFilter(userid=update.message.from_user.id))
 	if user == None:
 		new_user = utils.user.User.from_telegram(update.message.from_user)
