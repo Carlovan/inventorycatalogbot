1d5b8792504d661e10764c890a238bbcc6d9d13b
Carlovan <giulio.carlassare@gmail.com>
2017-05-20 23:29:19 +0200

    Fixed bug in /start command

--- commands/start.py
+++ commands/start.py
@@ -11,5 +11,4 @@ pass_args = False
 
 def run(bot, update):
 	update.message.reply_text('Ciao benvenuto nel catalogo di @InventoryBot! Ti consiglio caldamente di leggere /help per sapere come funziona :P')
-	update.message.reply_text(settings.changelog, parse_mode='HTML')
 	logger.info('User {} just started the bot'.format(update.message.from_user.username))
