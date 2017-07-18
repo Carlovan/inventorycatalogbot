44c23c1d716eefe90b42dbbba7d7595ecfe994bc
Carlovan <giulio.carlassare@gmail.com>
2017-07-18 17:29:22 +0200

    A bit of refactoring

--- utils/inventory.py
+++ utils/inventory.py
@@ -73,35 +73,34 @@ def add(inv):
 	dbitems.add(items)
 	return len(items)
 
+def add_user_items(inv, user_stateadding, item_state):
+	dbusers = database.users.DbUsers()
+	dbuseritems = database.user_items.DbUserItems()
+	user_state = inv.user.state
+	inv.user.state = user_stateadding
+	dbusers.update(inv.user)
+	dbuseritems.add_inventory(inv, item_state)
+	inv.user.state = user_state
+	dbusers.update(inv.user)
+	return 'Ok. Usa /fine, /annulla o manda altri oggetti.'
+
+
 def received(inv):
 	# Function to call when an inventory is received from the user.
 	# Then this function will choose what action to perform on that inventory
 	assert(type(inv) is Inventory)
 	count = add(inv)
 	inv.ensure_data()
-	dbusers = database.users.DbUsers()
 	try:
 		if inv.user.state == UserState.NONE:
 			logger.info('User {} added {} items'.format(inv.user.username, count))
 			return f'Hai aggiunto {count} oggetti.'
 		elif inv.user.state == UserState.CONFRONTA:
 			logger.info('User {} added items to confronta'.format(inv.user.username))
-			dbuseritems = database.user_items.DbUserItems()
-			inv.user.state = UserState.CONFRONTA_ADDING
-			dbusers.update(inv.user)
-			dbuseritems.add_inventory(inv, ItemState.CONFRONTA)
-			inv.user.state = UserState.CONFRONTA
-			dbusers.update(inv.user)
-			return 'Ok. Usa /fine, /annulla o manda altri oggetti.'
+			return add_user_items(inv, UserState.CONFRONTA_ADDING, ItemState.CONFRONTA)
 		elif inv.user.state == UserState.CONTAINV:
 			logger.info('User {} added item to containv'.format(inv.user.username))
-			dbuseritems = database.user_items.DbUserItems()
-			inv.user.state = UserState.CONTAINV_ADDING
-			dbusers.update(inv.user)
-			dbuseritems.add_inventory(inv, ItemState.CONTAINV)
-			inv.user.state = UserState.CONTAINV
-			dbusers.update(inv.user)
-			return 'Ok. Usa /fine, /annulla o manda altri oggetti.'
+			return add_user_items(inv, UserState.CONTAINV_ADDING, ItemState.CONTAINV)
 		elif inv.user.state in [UserState.CONFRONTA_ADDING, UserState.CONTAINV_ADDING]:
 			return 'Aspetta il messaggio di conferma!'
 	except Exception as ex:
