5c20f3303a5c68685661bbf46a1f8d6d5cafb314
Carlovan <giulio.carlassare@gmail.com>
2017-07-18 17:38:36 +0200

    Another bit of refactoring

--- commands/annulla.py
+++ commands/annulla.py
@@ -9,19 +9,18 @@ pass_args = False
 
 def run(bot, update):
 	dbusers = database.users.DbUsers()
+	dbuseritems = database.user_items.DbUserItems()
+
 	user = dbusers.get_single(UserFilter(userid=update.message.from_user.id))
+
 	if user.state == UserState.CONFRONTA:
-		dbuseritems = database.user_items.DbUserItems()
-		user.state = UserState.NONE
-		user.other = None
-		dbuseritems.clear(user)
-		dbusers.update(user)
 		update.message.reply_text('Comando <code>confronta</code> annullato.', parse_mode='HTML')
 	elif user.state == UserState.CONTAINV:
-		dbuseritems = database.user_items.DbUserItems()
-		user.state = UserState.NONE
-		dbuseritems.clear(user)
-		dbusers.update(user)
 		update.message.reply_text('Comando <code>containv</code> annullato.', parse_mode='HTML')
 	elif user.state in [UserState.CONFRONTA_ADDING, UserState.CONTAINV_ADDING]:
 		update.message.reply_text('Aspetta il messaggio di conferma!')
+
+	user.state = UserState.NONE
+	user.other = None
+	dbuseritems.clear(user)
+	dbusers.update(user)
